8a3419156eb9a3382924e53a17c1a2e76feb49b5
noahpetrides <nopetrides@hotmail.com>
2021-01-30 14:35:05 -0500

    Scene setups for prefabs and zones

---
+++ Catmin/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private ZoneManager[] Zones = null;
+
+    [SerializeField] private ZoneManager StarterZone = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StarterZone.SetUIForZone();
+    }
+}
---
+++ Catmin/Assets/Scripts/ZoneManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] CatsToSave = null;
+    public int CatCount => CatsToSave.Length;
+    private int CatsSaved = 0;
+
+    public void CatSaved()
+    {
+        CatsSaved++;
+    }
+
+    public void SetUIForZone()
+    {
+        
+    }
+}
